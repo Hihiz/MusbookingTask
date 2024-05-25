262749c5a9284916adc9fa4dccee4d9051cd8ec1
Hihiz <public@mail.ru>
2024-05-25 21:21:25 +0300

    Добавить HttpPost

--- MusbookingTask.Api/Controllers/EquipmentController.cs
+++ MusbookingTask.Api/Controllers/EquipmentController.cs
@@ -1,5 +1,7 @@
-﻿using MediatR;
+﻿using FluentValidation.Results;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using MusbookingTask.Application.Equipments.Commands.CreateEquipment;
 using MusbookingTask.Application.Equipments.Queries.GetEquipmentList;
 
 namespace MusbookingTask.Api.Controllers
@@ -14,5 +16,17 @@ namespace MusbookingTask.Api.Controllers
 
         [HttpGet]
         public async Task<ActionResult> GetEquipments(CancellationToken cancellationToken) => Ok(await _mediator.Send(new GetEquipmentListQuery(), cancellationToken));
+
+        [HttpPost]
+        public async Task<ActionResult> CreateEquipment(CreateEquipmentCommand command, CancellationToken cancellationToken)
+        {
+            ValidationResult result = new CreateEquipmentCommandValidator().Validate(command);
+            if (!result.IsValid)
+            {
+                return BadRequest(string.Join('\n', result.Errors));
+            }
+
+            return Ok(await _mediator.Send(command, cancellationToken));
+        }
     }
 }
