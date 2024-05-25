ea61619b80d9fb4dd12f3de68e635da7b201e812
Hihiz <public@mail.ru>
2024-05-25 20:49:46 +0300

    Добавить базовый репозиторий, контекст данных

---
+++ MusbookingTask.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusbookingTask.Domain.Entities;
+
+namespace MusbookingTask.Application.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Equipment> Equipments { get; set; }
+        public DbSet<OrderEquipment> OrderEquipments { get; set; }
+    }
+}
---
+++ MusbookingTask.Application/Interfaces/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace MusbookingTask.Application.Interfaces
+{
+    public interface IBaseRepository<T>
+    {
+        Task<List<T>> GetAll(CancellationToken cancellationToken = default);
+        Task<T> GetById(int id, CancellationToken cancellationToken = default);
+        Task<T> Create(T entity, CancellationToken cancellationToken = default);
+        Task<T> Update(T entity, CancellationToken cancellation = default);
+        Task Delete(T entity);
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
+
+        Task Attach(T entity, CancellationToken cancellationToken = default);
+    }
+}
