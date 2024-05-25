1d2ca2dcbd93eeebbd6f5407633823d19496319f
Hihiz <public@mail.ru>
2024-05-25 20:51:42 +0300

    Добавить класс контекста данных

---
+++ MusbookingTask.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusbookingTask.Application.Interfaces;
+using MusbookingTask.Domain.Entities;
+
+namespace MusbookingTask.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Equipment> Equipments { get; set; }
+        public DbSet<OrderEquipment> OrderEquipments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OrderEquipment>()
+                .HasKey(oe => new { oe.OrderId, oe.EquipmentId });
+
+            modelBuilder.Entity<OrderEquipment>()
+                .HasOne(oe => oe.Order)
+                .WithMany(o => o.OrderEquipments)
+                .HasForeignKey(oe => oe.OrderId);
+
+            modelBuilder.Entity<OrderEquipment>()
+                .HasOne(oe => oe.Equipment)
+                .WithMany(e => e.OrderEquipments)
+                .HasForeignKey(oe => oe.EquipmentId);
+        }
+    }
+}
---
+++ MusbookingTask.Infrastructure/Data/SeedData.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MusbookingTask.Domain.Entities;
+
+namespace MusbookingTask.Infrastructure.Data
+{
+    public static class SeedData
+    {
+        public static async void Initialize(IServiceProvider serviceProvider)
+        {
+            using (ApplicationDbContext db = new ApplicationDbContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                List<Order> orders = new List<Order>()
+                {
+                    new Order
+                    {
+                        Id = 1,
+                      Description = "Order1",
+                      CreatedAt = DateTime.Now,
+                      UpdatedAt = null,
+                      Price = 1000,
+                    },
+                    new Order
+                    {
+                       Id = 2,
+                      Description = "Order1",
+                      CreatedAt = DateTime.Now,
+                      UpdatedAt = null,
+                      Price = 1000,
+                    },
+                    new Order
+                    {
+                       Id = 3,
+                      Description = "Order1",
+                      CreatedAt = DateTime.Now,
+                      UpdatedAt = null,
+                      Price = 1000,
+                    }
+                };
+
+                await db.Orders.AddRangeAsync(orders);
+                
+                List<Equipment> equipments = new List<Equipment>()
+                {
+                    new Equipment
+                    {
+                         Id = 1,
+                       Name = "Микшерный пульт с процессором эффектов",
+                       Amount = 2,
+                       Price = 3000,
+                    },
+                    new Equipment
+                    {
+                           Id = 2,
+                       Name = "Мониторная система",
+                       Amount = 10,
+                       Price = 1500,
+                    },
+                    new Equipment
+                    {
+                          Id = 3,
+                       Name = "Микрофон",
+                       Amount = 2,
+                       Price = 2000,
+                    }
+                };
+
+                await db.Equipments.AddRangeAsync(equipments);
+
+                await db.OrderEquipments.AddRangeAsync(
+                    new OrderEquipment
+                    {
+                        OrderId = orders[0].Id,
+                        EquipmentId = equipments[0].Id,
+                        Quantity = 1
+                    },
+                      new OrderEquipment
+                      {
+                          OrderId = orders[1].Id,
+                          EquipmentId = equipments[1].Id,
+                          Quantity = 2
+                      },
+                        new OrderEquipment
+                        {
+                            OrderId = orders[2].Id,
+                            EquipmentId = equipments[2].Id,
+                            Quantity = 3
+                        }
+                    );
+
+                await db.SaveChangesAsync(new CancellationToken());
+            }
+        }
+    }
+}
